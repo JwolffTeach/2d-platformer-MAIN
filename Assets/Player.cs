373c84df295fa219a7948e577fa1e1f66c53b276
Jeremy Wolff <jwolff@dsdmail.net>
2018-05-01 09:30:50 -0600

    Added running animation and sprite faces correct direction (SpriteRenderer.flipX).

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,11 +11,14 @@ public class Player : MonoBehaviour {
     public bool isGrounded = false;
 
     private Animator animator;
+    private SpriteRenderer sprite;
+    [SerializeField] float runAnimationSpeed = 4;
     
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-	}
+        sprite = GetComponent<SpriteRenderer>();
+    }
 	
 	void Update () {
         MovementControl();
@@ -25,13 +28,30 @@ public class Player : MonoBehaviour {
 
     private void setAnimations() {
         animator.SetBool("isGrounded", isGrounded);
-        animator.SetFloat("Speed", rb.velocity.x);
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+        // Check if we are facing left or right, change the sprite direction accordingly.
+        if(rb.velocity.x >= 0) {    // Moving Right
+            sprite.flipX = false;   // Face Right
+        }
+        else {                      // Moving Left
+            sprite.flipX = true;    // Face Left
+        }
+
+        // Increase the animation speed when we are running.
+        if (Mathf.Abs(rb.velocity.x) >= 10) {
+            animator.speed = runAnimationSpeed;
+        }
+        else {
+            animator.speed = 1;
+        }
         animator.SetFloat("vSpeed", rb.velocity.y);
     }
 
     private void MovementControl() {
         float h = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(h * moveSpeed, rb.velocity.y);
+        print(rb.velocity.x);
     }
 
     private void JumpControl() {
