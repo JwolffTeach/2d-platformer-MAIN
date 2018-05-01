b3f24ad93bab218bee32efca323958ebf9091343
Jeremy Wolff <jwolff@dsdmail.net>
2018-04-30 18:32:10 -0600

    Imported sprites for character and platform. Added Coin Sprite.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -26,15 +26,11 @@ public class Player : MonoBehaviour {
     private void JumpControl() {
         if (isGrounded && Input.GetButtonDown("Jump")) {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            isGrounded = false;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
             isGrounded = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D collision) {
-            isGrounded = false;
-        
     }
 }
