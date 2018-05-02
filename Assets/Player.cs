e2b5ad00248c84405a7841b699d23c16bc2d3279
Jeremy Wolff <jwolff@dsdmail.net>
2018-05-02 14:05:09 -0600

    Refactored Dash code. Added a variable to control dash animation speed.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -16,9 +16,9 @@ public class Player : MonoBehaviour {
 
     // Ability stuff
     [SerializeField] bool dashing = false;
-    [SerializeField] float dashMultiplier;
-    [SerializeField] float dashLength;
-
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashDistance;
+    [SerializeField] float dashAnimationSpeed;
 
     void Start () {
         rb = GetComponent<Rigidbody2D>();
@@ -36,42 +36,47 @@ public class Player : MonoBehaviour {
         animator.SetBool("isGrounded", isGrounded);
         animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
 
-        // Check if we are facing left or right, change the sprite direction accordingly.
-        if(rb.velocity.x >= 0) {    // Moving Right
-            sprite.flipX = false;   // Face Right
-        }
-        else {                      // Moving Left
-            sprite.flipX = true;    // Face Left
-        }
+        setSpriteDirection();
 
-        // Increase the animation speed when we are running.
-        if (Mathf.Abs(rb.velocity.x) >= 10) {
+        // Set animation speed to something specific if we're dashing.
+        if (dashing) {
+            animator.speed = dashAnimationSpeed;
+        }
+        else if(Mathf.Abs(rb.velocity.x) >= 10) { // Increase the animation speed when we are running.
             animator.speed = runAnimationSpeed;
         }
         else {
             animator.speed = 1;
         }
+        // Increase the animation speed when we are running.
         animator.SetFloat("vSpeed", rb.velocity.y);
+    }
+
+    private void setSpriteDirection() {
+        // Check if we are facing left or right, change the sprite direction accordingly.
+        if (rb.velocity.x >= 0) {    // Moving Right
+            sprite.flipX = false;   // Face Right
+        }
+        else {                      // Moving Left
+            sprite.flipX = true;    // Face Left
+        }
     }
 
     private void MovementControl() {
         float h = Input.GetAxis("Horizontal");
         if (Input.GetKeyDown("g") && !dashing) { // We dash if we aren't dashing and hit the "g" key
-            StartCoroutine(doDash(h, dashLength));
+            StartCoroutine(doDash(h, dashDistance));
         }
         if (!dashing) { // We're not dashing, so move normal.
             rb.velocity = new Vector2(h * moveSpeed, rb.velocity.y);
         }
-        //else {
-        //   rb.velocity = new Vector2(h * moveSpeed, rb.velocity.y);
-        //}
     }
 
     IEnumerator doDash(float h, float waitTime) {
-        print("doing dash?");
         dashing = true;
+        isGrounded = true; // Set this to true so we can dash in the air.
         animator.SetBool("Dashing", dashing);
-        rb.velocity = new Vector2(h * moveSpeed * dashMultiplier, rb.velocity.y);
+        rb.velocity = new Vector2(h * moveSpeed * dashSpeed, rb.velocity.y);
         yield return new WaitForSeconds(waitTime);
         dashing = false;
         animator.SetBool("Dashing", dashing);
