f8bc8794a21906ffb3dd24f1e19740f08c44cfdf
Jeremy Wolff <jwolff@dsdmail.net>
2018-05-07 14:49:42 -0600

    Added a weapon component and bullet script. Character can fine bullets now.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    // Bullet Stats
+    [SerializeField] float speed;
+    float carrotOffset;
+    Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+        carrotOffset = transform.rotation.z;
+        transform.localRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 45f);
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddRelativeForce(Vector3.right  * speed, ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    [SerializeField] GameObject bullet;
+
+    // Weapon Stats
+    [SerializeField] float cooldown;
+    [SerializeField] float cooldownTimer;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cooldownTimer <= 0) {
+            if (Input.GetButtonDown("Fire1")) {
+                GameObject newBullet = Instantiate(bullet, transform.position, bullet.transform.rotation);
+                cooldownTimer = cooldown;
+            }
+        }
+        else {
+            cooldownTimer -= Time.deltaTime;
+        }
+	}
+}
